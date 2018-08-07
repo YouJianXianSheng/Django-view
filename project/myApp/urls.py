4cecd4a0a10205b1bc3963266ab6c5681a5c0c99
zcs <zcs@163.com>
2018-08-07 21:45:26 +0800

    2018-08-07

--- project/myApp/urls.py
+++ project/myApp/urls.py
@@ -1,31 +1,38 @@
-# ！/usr/bin/env python
-# -*- coding:utf-8 -*-
+#！/usr/bin/env python
+#-*- coding:utf-8 -*-
 # Author:zhangcs
-from django.conf.urls import url, include
+from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-    # 路由至主页
+	#路由至主页
     url(r'^$', views.Index),
-    url(r'^index.html/$', views.Index1),
-    url(r'^(\d+)/$', views.detail),
-    # 路由至grades
-    url(r'^grades/$', views.grades),
-    # 路由至students
-    url(r'^students/$', views.students),
-    # 查看属性
-    url(r'^attribles/$', views.attribles),
-    # 使用get获取网址中的值
-    url(r'^get1/$', views.get1),
-    url(r'^get2/$', views.get2),
-    # 使用post获取网址中的值
-    # 显示注册界面
-    url(r'^showregistered/$', views.showregistered),
-    url(r'^showregistered/registered/$', views.registered),
-    # 测试response属性
-    url(r'^response/$', views.response),
-    # 测试cookie
-    url(r'^cookieTest/$', views.cookieTest),
-    url(r'^redirect1/$', views.redirect1),
-    url(r'^redirect2/$', views.redirect2),
+	url(r'^index.html/$',views.Index1),
+	url(r'^(\d+)/$',views.detail),
+	#路由至grades
+	url(r'^grades/$',views.grades),
+	#路由至students
+	url(r'^students/$',views.students),
+	#查看属性
+	url(r'^attribles/$',views.attribles),
+	#使用get获取网址中的值
+	url(r'^get1/$',views.get1),
+	url(r'^get2/$',views.get2),
+	#使用post获取网址中的值
+	#显示注册界面
+	url(r'^showregistered/$',views.showregistered),
+	url(r'^showregistered/registered/$',views.registered),
+	#测试response属性
+	url(r'^response/$',views.response),
+	#测试cookie
+	url(r'^cookieTest/$',views.cookieTest),
+	url(r'^redirect1/$',views.redirect1),
+	url(r'^redirect2/$',views.redirect2),
+	#测试会话
+	#登录页面
+	url(r'^main/$',views.main),
+	url(r'^login/$',views.login),
+	url(r'^showmain/$',views.showmain),
+	#退出
+	url(r'^quit/$',views.quit),
 ]
--- project/myApp/views.py
+++ project/myApp/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Grades, Students
 
 
 # Create your views here.
 def Index(request):
     return HttpResponse("Hello Python!!!!")
-
 def Index1(request):
     return HttpResponseRedirect('/sunck/')
 
 def detail(request, num):
     return HttpResponse("detail-%s" % num)
-
 
 def grades(request):
     # 从模型中获取数据
@@ -21,13 +18,11 @@ def grades(request):
     # 将数据返回至grades.html
     return render(request, "myApp/grades.html", {"grades": gradesList})
 
-
 def students(request):
     # 从模型中获取数据
     studentsList = Students.objects.all()
     # 将数据返回至grades.html
     return render(request, "myApp/students.html", {"students": studentsList})
-
 
 # 查看属性
 def attribles(request):
@@ -42,7 +37,6 @@ def attribles(request):
 
     return HttpResponse('attribles')
 
-
 # 用get获取网址中的值
 def get1(request):
     a = request.GET.get('a')
@@ -50,7 +44,6 @@ def get1(request):
     c = request.GET.get('c')
 
     return HttpResponse(a + b + c)
-
 
 def get2(request):
     a = request.GET.getlist('a')
@@ -61,45 +54,63 @@ def get2(request):
 
     return HttpResponse(a1 + a2 + b + c)
 
-
 # 用post获取网址中值
 # 显示注册界面
 def showregistered(request):
     return render(request, 'myApp/registered.html')
-
 
 def registered(request):
     name = request.POST.get("name")
     gener = request.POST.get("gener")
     age = request.POST.get("age")
     hobby = request.POST.get("hobby")
-    print(name + gener + age + hobby)
+    print(name+gener+age+hobby)
     return HttpResponse("post成功")
 
-
-# 测试response的属性
+#测试response的属性
 def response(request):
     res = HttpResponse()
     res.content = b'good'
     print(res.content)
-    print(res.content - type)
+    print(res.content-type)
     print(res.charset)
     print(res.status_code)
 
     return res
 
-# 测试cookie
+#测试cookie
 def cookieTest(request):
     res = HttpResponse()
     cookie = request.COOKIES
-    res.write('<h1>' + cookie['name'] + '</h1>')
-    # cookie = res.set_cookie('name','zcs')
+    res.write('<h1>'+cookie['name']+'</h1>')
+    #cookie = res.set_cookie('name','zcs')
     return res
 
-# 测试重定向
+#测试重定向
 def redirect1(request):
-    #return HttpResponseRedirect('/sunck/redirect2/')
-    return redirect('/sunck/redirect2/')
+    return HttpResponseRedirect('/sunck/redirect2/')
 
 def redirect2(request):
     return HttpResponse('重定向成功')
+
+#测试会话
+#登录页面
+def main(request):
+    #获取session中用户名，没有用户名则显示“游客”
+    username = request.session.get('username',"游客")
+    return render(request,'myApp/main.html',{'username':username})
+
+def login(request):
+    return render(request,'myApp/login.html')
+
+def showmain(request):
+    #获取用户名
+    name = request.POST.get('username')
+    #设置session键对应获取的用户名
+    request.session['username']=name
+    #设置过期时间，为10秒
+    #request.session.set_expiry(10)
+    return HttpResponseRedirect('/sunck/main/')
+def quit(request):
+    request.session.flush()
+    return HttpResponseRedirect('/sunck/main/')
